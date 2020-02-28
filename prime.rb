21efa74febb74d01b1860c05ce610a8ad089daa4
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-28 03:56:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+    counter = 2
+    result = " "
+    while counter < num
+        float = num % counter == 0
+        if float == true
+            result = false
+    end
+        counter += 1
+    end
+    result == false || num < 2 ? false : true
+end
